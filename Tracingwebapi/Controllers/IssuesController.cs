3defa841536fe8e64d1f335fd75b8638bf33a8f1
Alimkhan <olimxonnutfullaevich@gmail.com>
2022-09-24 16:32:14 +0500

    Updated

---
+++ Tracingwebapi/Controllers/IssuesController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Tracingwebapi.Data;
+using Tracingwebapi.Models;
+
+namespace Tracingwebapi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class IssuesController : ControllerBase
+    {
+        private readonly IssueDbContext _context;
+
+        public IssuesController(IssueDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<Issue>> Get()
+        {
+            return await _context.Issues.ToListAsync();
+        }
+    }
+}
---
+++ Tracingwebapi/Data/IssueDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Tracingwebapi.Models;
+
+namespace Tracingwebapi.Data
+{
+    public class IssueDbContext : DbContext
+    {
+        public IssueDbContext(DbContextOptions<IssueDbContext> options) : base(options){ }
+
+        public DbSet<Issue> Issues { get; set; }
+    }
+}
