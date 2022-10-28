fdc8f4d09a535b2990f9a6ed2de87b3d9188fa73
Alimkhan <olimxonnutfullaevich@gmail.com>
2022-10-28 20:36:22 +0500

    await

--- Async Prog/Program.cs
+++ Async Prog/Program.cs
@@ -2,14 +2,31 @@
 {
     class Program
     {
+        static async Task<string> ReadFileAsync(string file)
+        {
+            string content = string.Empty;
+
+            using (StreamReader reader = new StreamReader(file))
+            {
+                content = await reader.ReadToEndAsync();
+            }
+
+            return content;
+        }
+
+        static async void ContentToConsole()
+        {
+            string readFileTask = await ReadFileAsync("c:\\temp\\file.txt");
+
+            Console.WriteLine("Fayldagi ma'lumotlar:" + readFileTask);
+        }
+
         static void Main(string[] args)
         {
-            int a;
-
-            foreach(char c in "banana")
-            {
-                Console.WriteLine(c);
-            }
+            Task task = new Task(ContentToConsole);
+            task.Start();
+            task.Wait();
+            Console.ReadLine();
         }
     }
 }
