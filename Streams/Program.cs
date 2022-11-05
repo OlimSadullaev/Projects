1cdd6ef9222fe6336eee9b95d387b3bfa1afc6a1
Alimkhan <olimxonnutfullaevich@gmail.com>
2022-11-05 17:22:50 +0500

    message

---
+++ Streams/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.IO;
+using System.Text;
+
+namespace Streams
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           // var movieDataManager = new MovieDataManager();
+            //var movies = movieDataManager.GetAllMovies();
+
+           /* foreach(var movie in movies)
+            {
+                Console.WriteLine(movie.Title);
+            }*/
+
+            FileStream F = new FileStream(@"C:\\Users\\user\\Desktop\\file\\salam.txt", FileMode.OpenOrCreate);
+            Console.WriteLine("Enter the text : ");
+
+            string s = Console.ReadLine();
+            byte []ascii = Encoding.ASCII.GetBytes(s);
+
+            foreach(byte item in ascii)
+            {
+                F.WriteByte(item);
+            }
+
+            Console.Write("Data successfully written named as salam.txt");
+
+            F.Close();
+        }
+    }
+}
