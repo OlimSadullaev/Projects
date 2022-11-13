5aef2b071f9d9d4cd324ca0491d100ad82028e73
Alimkhan <olimxonnutfullaevich@gmail.com>
2022-11-13 17:43:56 +0500

    messa

--- Streams/Class1.cs
+++ Streams/Class1.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,29 @@ namespace Streams
 {
     internal class Class1
     {
+        public string FullName { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+
+        public virtual void AccessToAddAdmin()
+        {
+            Console.WriteLine("Guruhga admin qo'shish uchun ruxsat berilgan");
+        }
+
+        public virtual void AccessToChangeGroupInfo()
+        {
+            Console.WriteLine("Guruh ma'lumotlarini o'zgartirish uchun ruxsat berilgan");
+        }
+
+        public virtual void AccessToReadMessages()
+        {
+            Console.WriteLine("Xabarlarni o'qish uchun ruxsat berilgan");
+        }
+        public virtual void AccessToRemoveMessages()
+        {
+            Console.WriteLine("Xabarlarni o'chirish uchun ruxsat berilgan");
+        }
+
+
     }
 }
