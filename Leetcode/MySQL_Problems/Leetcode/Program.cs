d6361ed0aa2c47bba7753e11b2dcfb238d19a65e
Alimkhan <olimxonnutfullaevich@gmail.com>
2022-11-14 16:56:07 +0500

    problems

--- Leetcode/MySQL_Problems/Leetcode/Program.cs
+++ Leetcode/MySQL_Problems/Leetcode/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var c=0;
+ c = int.Parse(Console.ReadLine());
+ Console.WriteLine("Sun Mon Tue Wed Thu Fri Sat");
+ Console.Write($"{c,3}");
+
+ /*c = c % 31 + 1;
+ Console.Write($"{c,4}");
+
+ c = c % 31 + 1;
+ Console.Write($"{c,4}");
+
+ c = c % 31 + 1;
+ Console.Write($"{c,4}");
+
+ c = c % 31 + 1;
+ Console.Write($"{c,4}");
+
+ c = c % 31 + 1;
+ Console.Write($"{c,4}");
+
+ c = c % 31 + 1;
+ Console.Write($"{c,4}");*/
+for(int i = 0; i < 6; i++)
+{
+    c = c % 31 + 1;
+    Console.Write($"{c,4}");
+}
