f73ceebdb099e87f91f303f7ee216c2086862d13
Alimkhan <olimxonnutfullaevich@gmail.com>
2022-11-14 18:55:44 +0500

    Collections

--- Leetcode/MySQL_Problems/Leetcode/Program.cs
+++ Leetcode/MySQL_Problems/Leetcode/Program.cs
@@ -1,11 +1,10 @@
-﻿var c=0;
+﻿using System.Collections;
+
+/*var c=0;
  c = int.Parse(Console.ReadLine());
  Console.WriteLine("Sun Mon Tue Wed Thu Fri Sat");
  Console.Write($"{c,3}");
 
- /*c = c % 31 + 1;
- Console.Write($"{c,4}");
-
  c = c % 31 + 1;
  Console.Write($"{c,4}");
 
@@ -19,9 +18,48 @@
  Console.Write($"{c,4}");
 
  c = c % 31 + 1;
- Console.Write($"{c,4}");*/
+ Console.Write($"{c,4}");
+
+ c = c % 31 + 1;
+ Console.Write($"{c,4}");
 for(int i = 0; i < 6; i++)
 {
     c = c % 31 + 1;
     Console.Write($"{c,4}");
-}
+}*/
+
+/*Hashtable numberNames = new Hashtable();
+numberNames.Add(1, "One"); //adding a key/value using the Add() method
+numberNames.Add(2, "Two");
+numberNames.Add(3, "Three");
+
+foreach (DictionaryEntry de in numberNames)
+    Console.WriteLine("Key: {0}, Value: {1}", de.Key, de.Value);*/
+
+/*Stack<int> myStack = new Stack<int>();
+myStack.Push(1);
+myStack.Push(2);
+myStack.Push(3);
+myStack.Push(4);
+// myStack.Contains(5);
+
+foreach(var item in myStack)
+    Console.WriteLine(item + ",");
+
+foreach (var item in myStack)
+    Console.Write(item + ","); //prints 4,3,2,1, 
+/*Console.WriteLine("------------------------");
+Console.WriteLine(myStack.Peek());
+Console.WriteLine(myStack.Clear);*/
+Hashtable numberNames = new Hashtable();
+numberNames.Add(1, "One"); //adding a key/value using the Add() method
+numberNames.Add(2, "Two");
+numberNames.Add(3, "Three");
+
+//The following throws run-time exception: key already added.
+//numberNames.Add(3, "Three"); 
+
+foreach (DictionaryEntry de in numberNames)
+    Console.WriteLine("Key: {0}, Value: {1}", de.Key, de.Value);
+
+
