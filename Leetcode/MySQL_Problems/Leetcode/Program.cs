4fda9a61a7046497a14630183ba8c025e9ec155a
Alimkhan <olimxonnutfullaevich@gmail.com>
2022-11-15 16:42:09 +0500

    collections

--- Leetcode/MySQL_Problems/Leetcode/Program.cs
+++ Leetcode/MySQL_Problems/Leetcode/Program.cs
@@ -51,7 +51,8 @@ foreach (var item in myStack)
 /*Console.WriteLine("------------------------");
 Console.WriteLine(myStack.Peek());
 Console.WriteLine(myStack.Clear);*/
-Hashtable numberNames = new Hashtable();
+//---------------------------------
+/*Hashtable numberNames = new Hashtable();
 numberNames.Add(1, "One"); //adding a key/value using the Add() method
 numberNames.Add(2, "Two");
 numberNames.Add(3, "Three");
@@ -60,6 +61,21 @@ numberNames.Add(3, "Three");
 //numberNames.Add(3, "Three"); 
 
 foreach (DictionaryEntry de in numberNames)
-    Console.WriteLine("Key: {0}, Value: {1}", de.Key, de.Value);
+    Console.WriteLine("Key: {0}, Value: {1}", de.Key, de.Value);*/
+//----------------------------------
+/*ArrayList arlist = new ArrayList();
+// or 
+var arrlist = new ArrayList(); // recommended 
+*/
 
+/*Tuple<int, string, string> person = new Tuple<int, string, string>(1, "Steve", "Jobs");
+Console.WriteLine(person);
+*/
 
+var primes = Tuple.Create(2, 3, 5, 7, 11, 13, 17, 19);
+Console.WriteLine("Prime numbers less than 20: " +
+                  "{0}, {1}, {2}, {3}, {4}, {5}, {6}, and {7}",
+                  primes.Item1, primes.Item2, primes.Item3,
+                  primes.Item4, primes.Item5, primes.Item6,
+                  primes.Item7, primes.Rest.Item1);
+Console.WriteLine(primes);
