6094f5844f7f41c1fcb7ccd51505f51bf38052c4
Alimkhan <olimxonnutfullaevich@gmail.com>
2022-11-17 18:11:47 +0500

    streams

--- Leetcode/MySQL_Problems/Leetcode/Program.cs
+++ Leetcode/MySQL_Problems/Leetcode/Program.cs
@@ -158,7 +158,7 @@ foreach (var kvp in numberNames)
     Console.WriteLine("key: {0}, value: {1}", kvp.Key, kvp.Value);
 Console.ReadKey();*/
 
-class Program
+/*class Program
 {
     static void WriteFile(string Filename)
     {
@@ -201,4 +201,21 @@ class Program
 
         Console.Read();
     }
-}
+
+    
+}*/
+
+FileInfo fi = new FileInfo(@"D:\DummyFile.txt");
+
+//Open a file for Read\Write
+FileStream fs = fi.Open(FileMode.OpenOrCreate, FileAccess.Read, FileShare.Read);
+
+//Create an object of StreamReader by passing FileStream object on which it needs to operates on
+StreamReader sr = new StreamReader(fs);
+
+//Use the ReadToEnd method to read all the content from file
+string fileContent = sr.ReadToEnd();
+
+//Close the StreamReader object after operation
+sr.Close();
+fs.Close();
