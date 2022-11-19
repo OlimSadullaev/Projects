e7dfc4eb8d38e9028478533582dcac88db147a0e
Alimkhan <olimxonnutfullaevich@gmail.com>
2022-11-19 12:52:17 +0500

    Example

--- Leetcode/MySQL_Problems/Leetcode/Program.cs
+++ Leetcode/MySQL_Problems/Leetcode/Program.cs
@@ -221,7 +221,7 @@ Console.ReadKey();*/
 // fs.Close();/*
 
 // Data source
-string[] names = { "Bill", "Steve", "James", "Mohan" };
+/*string[] names = { "Bill", "Steve", "James", "Mohan" };
 
 // LINQ Query 
 var myLinqQuery = from name in names
@@ -242,4 +242,42 @@ var linq = from car in cars
 foreach (var car in linq)
     Console.Write(car + ' ');
 
-Console.Read();
+Console.Read();*/
+
+class Student
+{
+    public int StudentID { get; set; }
+    public String StudentName { get; set; }
+    public int Age { get; set; }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Student[] studentArray = {
+            new Student() { StudentID = 1, StudentName = "John", Age = 18 },
+            new Student() { StudentID = 2, StudentName = "Steve",  Age = 21 },
+            new Student() { StudentID = 3, StudentName = "Bill",  Age = 25 },
+            new Student() { StudentID = 4, StudentName = "Ram" , Age = 20 },
+            new Student() { StudentID = 5, StudentName = "Ron" , Age = 31 },
+            new Student() { StudentID = 6, StudentName = "Chris",  Age = 17 },
+            new Student() { StudentID = 7, StudentName = "Rob",Age = 19  },
+        };
+
+        Student[] students = new Student[10];
+
+        int i = 0;
+
+        foreach (Student std in studentArray)
+        {
+            if (std.Age > 12 && std.Age < 20)
+            {
+                students[i] = std;
+                i++;
+            }
+            Console.WriteLine();
+        }
+
+    }
+}
