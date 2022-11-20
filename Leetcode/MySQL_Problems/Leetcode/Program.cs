2f8a9fb50f99d63b6408f8c46e9539702b747464
Alimkhan <olimxonnutfullaevich@gmail.com>
2022-11-20 16:36:51 +0500

    LINQ

--- Leetcode/MySQL_Problems/Leetcode/Program.cs
+++ Leetcode/MySQL_Problems/Leetcode/Program.cs
@@ -2,7 +2,7 @@
 using System.IO;
 using System.Text;
 
-var c=0;
+/*var c=0;
  c = int.Parse(Console.ReadLine());
  Console.WriteLine("Sun Mon Tue Wed Thu Fri Sat");
  Console.Write($"{c,3}");
@@ -344,4 +344,77 @@ var empDetail = from emp in e.EmployeeMasters
                 };*/
 
 
+/*IList<Student> studentList = new List<Student>() {
+    new Student() { StudentID = 1, StudentName = "John", Age = 18 } ,
+    new Student() { StudentID = 2, StudentName = "Steve",  Age = 15 } ,
+    new Student() { StudentID = 3, StudentName = "Bill",  Age = 25 } ,
+    new Student() { StudentID = 4, StudentName = "Ram" , Age = 20 } ,
+    new Student() { StudentID = 5, StudentName = "Ron" , Age = 19 }
+};
 
+var orderByResult = from s in studentList
+                    orderby s.StudentName
+                    select s;
+
+var orderByDescendingResult = from s in studentList
+                              orderby s.StudentName descending
+                              select s;*/
+/*
+IList<string> strList1 = new List<string>() {
+    "One",
+    "Two",
+    "Three",
+    "Four"
+};
+
+IList<string> strList2 = new List<string>() {
+    "One",
+    "Two",
+    "Five",
+    "Six"
+};
+
+var innerJoin = strList1.Join(strList2,
+                      str1 => str1,
+                      str2 => str2,
+                      (str1, str2) => str1);
+Console.WriteLine();
+
+public class Student
+{
+    public int StudentID { get; set; }
+    public string StudentName { get; set; }
+    public int StandardID { get; set; }
+}
+
+public class Standard
+{
+    public int StandardID { get; set; }
+    public string StandardName { get; set; }
+}
+class Program
+{
+        IList<Student> studentList = new List<Student>() {
+        new Student() { StudentID = 1, StudentName = "John", StandardID =1 },
+        new Student() { StudentID = 2, StudentName = "Moin", StandardID =1 },
+        new Student() { StudentID = 3, StudentName = "Bill", StandardID =2 },
+        new Student() { StudentID = 4, StudentName = "Ram" , StandardID =2 },
+        new Student() { StudentID = 5, StudentName = "Ron"  }
+    };
+
+        IList<Standard> standardList = new List<Standard>() {
+        new Standard(){ StandardID = 1, StandardName="Standard 1"},
+        new Standard(){ StandardID = 2, StandardName="Standard 2"},
+        new Standard(){ StandardID = 3, StandardName="Standard 3"}
+    };
+
+    var innerJoin = studentList.Join(// outer sequence 
+                          standardList,  // inner sequence 
+                          student => student.StandardID,    // outerKeySelector
+                          standard => standard.StandardID,  // innerKeySelector
+                          (student, standard) => new  // result selector
+                          {
+                              StudentName = student.StudentName,
+                              StandardName = standard.StandardName
+                          });
+}*/
