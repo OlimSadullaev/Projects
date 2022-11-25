39c22b31dc8fdb41b04a2a751d470e100efbd055
Alimkhan <olimxonnutfullaevich@gmail.com>
2022-11-25 18:48:38 +0500

    LINQ

--- Leetcode/MySQL_Problems/Leetcode/Program.cs
+++ Leetcode/MySQL_Problems/Leetcode/Program.cs
@@ -457,7 +457,7 @@ var newList = strList.Skip(3);
 foreach (var str in newList)
     Console.WriteLine(str);*/
 
-IList<string> strList = new List<string>() {
+/*IList<string> strList = new List<string>() {
                                             "One",
                                             "Two",
                                             "Three",
@@ -469,3 +469,41 @@ var resultList = strList.SkipWhile(s => s.Length < 5);
 
 foreach (string str in resultList)
     Console.WriteLine(str);
+
+public class Student
+{
+    public int StudentID { get; set; }
+    public string StudentName { get; set; }
+
+    public int Age { get; set; }
+
+    public int StandardID { get; set; }
+
+}
+public class Program
+{
+    IList<Student> studentList = new List<Student>() {
+    new Student() { StudentID = 1, StudentName = "John", Age = 18, StandardID = 1 } ,
+    new Student() { StudentID = 2, StudentName = "Steve",  Age = 21, StandardID = 1 } ,
+    new Student() { StudentID = 3, StudentName = "Bill",  Age = 18, StandardID = 2 } ,
+    new Student() { StudentID = 4, StudentName = "Ram" , Age = 20, StandardID = 2 } ,
+    new Student() { StudentID = 5, StudentName = "Ron" , Age = 21 }
+};
+
+
+}*/
+// string collection
+IList<string> stringList = new List<string>() {
+    "C# Tutorials",
+    "VB.NET Tutorials",
+    "Learn C++",
+    "MVC Tutorials" ,
+    "Java"
+};
+
+// LINQ Query Syntax
+var result = from s in stringList
+             where s.Contains("Tutorials")
+             select s;
+
+Console.WriteLine(result);
