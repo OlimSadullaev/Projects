489a3b8ad94d8165963cc6409e556030111a3b23
Alimkhan <olimxonnutfullaevich@gmail.com>
2022-12-02 19:47:24 +0500

    Pro

---
+++ SalesOrder/SalesOrder/Entities/Clothes.cs
@@ -0,0 +1,15 @@
+﻿namespace SalesOrder.Api.Entities
+{
+    public class Clothes
+    {
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Brend { get; set; }
+
+        public string Country { get; set; }
+
+        public int Price { get; set; }
+    }
+}
