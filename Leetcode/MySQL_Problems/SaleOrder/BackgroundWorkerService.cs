a68b80ac23f72bc76cf82db009982ce9272b2dcc
Alimkhan <olimxonnutfullaevich@gmail.com>
2022-12-04 18:42:40 +0500

    Background-Services

--- Leetcode/MySQL_Problems/SaleOrder/BackgroundWorkerService.cs
+++ Leetcode/MySQL_Problems/SaleOrder/BackgroundWorkerService.cs
@@ -1,3 +1,32 @@
-﻿public class BackgroundWorkerService
+﻿using System.Threading;
+
+public class BackgroundWorkerService : BackgroundService
 {
+    readonly ILogger<BackgroundWorkerService> _logger;
+
+    public BackgroundWorkerService(ILogger<BackgroundWorkerService> logger)
+    {
+        _logger = logger;
+    }
+
+    /*public async Task StartAsync(CancellationToken cancellationToken)
+    {
+        _logger.LogInformation("Service started.");
+        
+    }
+
+    public Task StopAsync(CancellationToken cancellationToken)
+    {
+        _logger.LogInformation("Service stopped.");
+        return Task.CompletedTask;
+    }*/
+
+    protected async override Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        while (!stoppingToken.IsCancellationRequested)
+        {
+            _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
+            await Task.Delay(1000, stoppingToken);
+        }
+    }
 }
