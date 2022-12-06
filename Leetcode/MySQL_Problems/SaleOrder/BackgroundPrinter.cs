69f8d1ba6569a90f2e37ed26d5d2f44126157dcc
Alimkhan <olimxonnutfullaevich@gmail.com>
2022-12-06 14:42:52 +0500

    123

---
+++ Leetcode/MySQL_Problems/SaleOrder/BackgroundPrinter.cs
@@ -0,0 +1,35 @@
+﻿namespace SaleOrder.API
+{
+    public class BackgroundPrinter : IHostedService, IDisposable
+    {
+        private readonly ILogger logger;
+        private int number = 0;
+        private Timer timer;
+
+        public BackgroundPrinter(ILogger<BackgroundPrinter> logger)
+        {
+            this.logger = logger;
+        }
+
+        public void Dispose()
+        {
+            timer?.Dispose();
+        }
+
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            timer = new Timer(o => logger.LogInformation("Printing From Worker"),
+                null,
+                TimeSpan.Zero,
+                TimeSpan.FromSeconds(5));
+
+            return Task.CompletedTask;
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            logger.LogInformation("Printing worker stopped");
+            return Task.CompletedTask;
+        }
+    }
+}
