0792bb0bbf0fef3eac26b16bd54785780480a4ce
Alimkhan <olimxonnutfullaevich@gmail.com>
2022-12-11 18:39:43 +0500

    proj

---
+++ SalesOrder/SalesOrder/Models/PhotoModel.cs
@@ -0,0 +1,16 @@
+﻿namespace SalesOrder.Api.Models
+{
+    public class PhotoModel
+    {
+        public IEnumerable<Guid> MediasId { get; set; }
+
+        public string BrendName { get; set; }
+
+        public string Country { get; set; }
+
+        public string Price { get; set; }
+
+        public string AvailableSize { get; set; }
+
+    }
+}
