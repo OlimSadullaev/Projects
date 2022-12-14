fd00ddb51ade42c3aaa66e13de44fa4121ae349e
Alimkhan <olimxonnutfullaevich@gmail.com>
2022-12-14 20:39:58 +0500

    Order

--- SalesOrder/SalesOrder/Data/DataDbContext.cs
+++ SalesOrder/SalesOrder/Data/DataDbContext.cs
@@ -1,9 +1,14 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using SalesOrder.Api.Entities;
 
 namespace SalesOrder.Api.Data
 {
     public class DataDbContext : DbContext
     {
-        
+        public DataDbContext(DbContextOptions<DataDbContext> options) : base(options) { }
+
+        public DbSet<Media> Medias { get; set; }
+
+        public DbSet<Photo> Photos { get; set; }
     }
 }
