fa23b356ec4cf59a64e439debdfb2fe7b8b070fa
Alimkhan <olimxonnutfullaevich@gmail.com>
2023-01-12 15:17:25 +0500

    DI

---
+++ DI(IoC container)/Employee.cs
@@ -0,0 +1,13 @@
+﻿namespace DI_IoC_container_
+{
+    public interface IEmployee
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+    public class Employee : IEmployee
+    {
+        public string Name { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string Description { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+    }
+}
